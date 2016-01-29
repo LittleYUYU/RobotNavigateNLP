310f72f42c86272bce4ce26f7419052a2a74a377
littleyuyu <ziyuyao123@gmail.com>
2016-01-29 15:37:38 -0500

    add project description

---
+++ hw0.R
@@ -0,0 +1,11 @@
+## Fill in the script below as appropriate.  Make sure it runs and
+## then submit the modified version to the carmen drop box.
+
+cat("\n")
+print(paste("My group includes:","Ziyu Yao,", "Shuang Li,", "Qi Zhu."))
+cat("\n")
+print(paste("We're all in section:", "12:45pm."))
+cat("\n")
+print(paste("Our project is titled:","Navigate robots with natural language instructions"))
+cat("\n")
+print(paste("Usually, robots can follow a set of machine instructions. For example, '(3,5,180) (3,7,270), ...' means that the robot walks from position(3,5) with orientation 180 degree to position(3,7), then it turns its orientation to 270 degree for the next instruction. However, this is unfriendly to human instructor since no one would talk with numbers. Therefore, in this project, we want to design a model to translate natural language (e.g., 'place your back against the wall of the “T” intersection') to machine interpretable instructions (corresponding to '(3,5,0),(3,5,180)'). "))
