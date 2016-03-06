371cbf6504ad30e92fe1568f3c30384ccd17a62a
MathBenDAI <cfdtlee@gmail.com>
2016-03-05 22:08:24 -0500

    add django framework as backend

---
+++ Implement/Implement/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+# from django.conf.urls.defaults import *
+from Implement.views import current_datetime
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Implement.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'Implement.views.index', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^time/$', current_datetime),
+    # url(r'^search/(?P<keyword>\w*)/$', 'Implement.views.judge'),
+    url(r'^add/$', 'Implement.views.add', name='add'),
+    url(r'^judge/$', 'Implement.views.judge', name='judge'),
+    url(r'^ajax_dict/$', 'Implement.views.ajax_dict', name='ajax-dict'),
+    url(r'^ajax_list/$', 'Implement.views.ajax_list', name='ajax-list'),
+)
---
+++ Implement/Implement/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+import json
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    a = int(a)
+    b = int(b)
+    return HttpResponse(str(a+b))
+
+def ajax_dict(request):
+    name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
+    return HttpResponse(json.dumps(name_dict), content_type='application/json')
+
+def ajax_list(request):
+    a = range(100)
+    return HttpResponse(json.dumps(a), content_type='application/json')
+
+def index(request):
+    return render(request, 'index.html')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def judge(request):
+    instruction = request.GET['instruction']
+    return HttpResponse(instruction)
+
+def search(request, keyword):
+    if request.session.get('UserID', False):
+        UserID = request.session['UserID']
+        UserType = request.session['UserType']
+        UserAccount = request.session['UserAccount']
+    else:
+        UserID = None
+        UserType = None
+        UserAccount = None
+    # commodityList = Commodity.objects.filter(CommodityName__contains = keyword)
+    # commodityList_by_amount = commodityList.order_by('SoldAmount')
+    # commodityList_by_price = commodityList.order_by('SellPrice')
+    # commodityList_by_counterprice = commodityList.order_by('-SellPrice')
+    return render_to_response('Customer_CommodityList.html', locals())
