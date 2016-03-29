234b2f6864ab943c61d351d325539074f12d71bb
MathBenDAI <cfdtlee@gmail.com>
2016-03-28 20:50:50 -0400

    finish drawing maps

--- Implement/assets/javascript/my_javascript.js
+++ Implement/assets/javascript/my_javascript.js
@@ -2,81 +2,105 @@
  * Created by eric on 3/24/16.
  */
 
-function draw_rect(x, y){
+function draw_rect(node){
+    var d = $("select option:selected")[0].value == "map_grid" ? 0 : 406;
+    x = node.attr("x")*30-d;
+    y = node.attr("y")*30-d;
     var context = $("#map")[0].getContext("2d");
-    context.fillStyle='#FF000';
-    context.strokeRect(x*30-405, y*30-405, 10, 10);
-}
+    context.fillStyle='lightgray';
+//    context.strokeStyle = 'black';
+    context.lineWidth = 1;
 
-function draw_edge(node1, node2){
+    context.strokeRect(x, y, 12, 12);
+    context.fillRect(x, y, 12, 12);
+    context.fillStyle = "black";
+    context.fillText(node.attr("item")[0] == null ? " " : node.attr("item")[0].toUpperCase(), x+3, y+10);
+}
+color_dict = {  "blue"      :"60b5e6",
+                "brick"     :"b32723",
+                "concrete"  :"918f90",
+                "flower"    :"e9168c",
+                "grass"     :"18a554",
+                "gravel"    :"6f6c9b",
+                "wood"      :"f4854f",
+                "yellow"    :"fcdc41",
+                "tower"     :"d7926b",
+                "butterfly" :"66cbdd",
+                "fish"      :"ee37a1"};
+function draw_edge(edge){
+    node1 = edge.attr("node1");
+    node2 = edge.attr("node2");
     var context = $("#map")[0].getContext("2d");
-    x1 = node1.split(",")[0]*30-400;
-    y1 = node1.split(",")[1]*30-400;
-    x2 = node2.split(",")[0]*30-400;
-    y2 = node2.split(",")[1]*30-400;
-    context.fillStyle='#FF000';
+    var d = $("select option:selected")[0].value == "map_grid" ? -6 : 400;
+    x1 = node1.split(",")[0]*30-d;
+    y1 = node1.split(",")[1]*30-d;
+    x2 = node2.split(",")[0]*30-d;
+    y2 = node2.split(",")[1]*30-d;
+    context.beginPath();
+    context.lineWidth = 12;
+    context.fillStyle = eval("color_dict." + edge.attr("wall"));
+    context.strokeStyle = color_dict[edge.attr("wall")];//eval("color_dict." + edge.attr("floor"));
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.lineWidth = 8;
+    context.fillStyle = eval("color_dict." + edge.attr("floor"));
+    context.strokeStyle = color_dict[edge.attr("floor")];//eval("color_dict." + edge.attr("floor"));
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+function draw_map(map_name){
+    $.get('assets/maps/'+ map_name +'.xml', function (d) {
+
+        var nodes = $(d).find('node');
+
+        // draw edges
+        var edges = $(d).find("edge");
+        edges.each(function(){
+            draw_edge($(this));
+        });
+
+        // draw nodes
+        nodes.each(function(){
+            var node = $(this);
+            draw_rect(node);
+        });
+    });
 }
 
 $(document).ready(function(){
-    $.get('assets/maps/map_one.xml', function (d) {
+//    $.get('assets/maps/map_one.xml', function (d) {
+//        var $map = $(d).find('map');
+//        var nodes = $(d).find('node');
+//
+//        // draw edges
+//        var edges = $(d).find("edge");
+//        edges.each(function(){
+//            draw_edge($(this));
+//        });
+//
+//        // draw nodes
+//        nodes.each(function(){
+//            var node = $(this);
+//            draw_rect(node.attr("x"),node.attr("y"));
+//        });
+//
+//    });
+    draw_map("map_one");
 
-
-        var $map = $(d).find('map');
-        var nodes = $(d).find('node');
-//        var canvas=document.getElementById('map');
-        var canvas = $("#map")[0];
-        var ctx=canvas.getContext('2d');
-        ctx.fillStyle='#FF0000';
-        n = $(d).find("node").first();
-
-        nodes.each(function(){
-            var node = $(this);
-            $("#node").append(node.attr("x"), ' ');
-            draw_rect(node.attr("x"),node.attr("y"));
-        });
-
-
-        $("#node").append('<br>')
-        nodes.each(function(){
-            var node = $(this);
-            $("#node").append(node.attr("y"), ' ');
-        });
-
-        var edges = $(d).find("edge");
-        edges.each(function(){
-            draw_edge($(this).attr("node1"), $(this).attr("node2"));
-        });
-//        alert(node.attr("y"));
-        var $nodes = $map.find('nodes');
-        $nodes.each(function() {
-            var $node = $(this);
-            var x = $node.attr("x");
-            var item = $node.attr("item");
-        });
-        $('#show').click(function(){
-            var canvas=document.getElementById('map');
-            var ctx=canvas.getContext('2d');
-            ctx.fillStyle='#FF0000';
-            ctx.fillRect(0,0,40,60);
-        });
-        $(d).find('book').each(function () {
-            var $book = $(this);
-            var title = $book.attr("title");
-            var description = $book.find('description').text();  //这里是读取字段内容
-            var imageurl = $book.attr('imageurl');     //这里是读取字段属性值
-
-            var html = '<dt> <img class="bookImage" alt="" src="' + imageurl + '" /> </dt>';
-            html += '<dd> <span class="loadingPic" alt="Loading" />';
-            html += '<p class="title">' + title + '</p>';
-            html += '<p> ' + description + '</p>';
-            html += '</dd>';
-            $('dl').append($(html));
-            $('.loadingPic').fadeOut(2000);
-        });
+    $("#map_select").change(function(){
+        var canvas = $("canvas")[0];
+        var context = $("#map")[0].getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        draw_map($("select option:selected")[0].value);
     });
+
     $("#judge").click(function(){
         var instruction = $("#instruction").val();
         $.get("/judge/",{'instruction':instruction}, function(ret){
